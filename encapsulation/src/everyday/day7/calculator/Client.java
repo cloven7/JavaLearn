0ef7213d56a07ad3968cbe99455bcfc719fb046d
cloven7 <1904887284@qq.com>
2022-07-07 15:43:43 +0800

    DAY07:JVM

---
+++ encapsulation/src/everyday/day7/calculator/Client.java
@@ -0,0 +1,36 @@
+package everyday.day7.calculator;
+
+/**
+ * @Description
+ * @Param [session]
+ * @Return java.lang.String
+ * @Throws
+ * @Author clovenine
+ * @Date 2022/7/7 9:31
+ * @Version 1.0
+ */
+public class Client {
+
+    public static int f = 0;
+    public static void juruo(){
+        System.out.println("ji tang lai luo !");
+    }
+    public Client(){
+        System.out.println("you are a pig !");
+    }
+
+    public static void main(String[] args) {
+//        new Client();
+//        Client.juruo();
+//        Client.f = 11;
+//        System.out.println(Client.f);
+
+        byte[] bytes = new byte[1024 * 1024];  bytes = null;
+        byte[] bytes1 = new byte[1024 * 1024];bytes1 = null;
+        byte[] bytes2 = new byte[1024 * 1024];bytes2 = null;
+        byte[] bytes3 = new byte[1024 * 1024];bytes3 = null;
+        byte[] bytes4 = new byte[1024 * 1024];bytes4 = null;
+
+    }
+
+}
